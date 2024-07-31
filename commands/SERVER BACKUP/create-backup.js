9c91eb163e8d251eb7cb13ada16e7e460fab58c8
ToxicDevil <93937235+ToxicDevilGamer@users.noreply.github.com>
2024-07-31 21:43:01 +0530

    Add files via upload

---
+++ commands/SERVER BACKUP/create-backup.js
@@ -0,0 +1,22 @@
+const backup = require("discord-backup");
+backup.setStorageFolder(__dirname + "/backup-json/");
+module.exports = {
+  name: "create-backup",
+  description: "Create Backup Servers",
+  P_user: ["ADMINISTRATOR"],
+  P_bot: ["MANAGE_ROLES","MANAGE_GUILD","MANAGE_EMOJIS","MANAGE_CHANNELS","VIEW_CHANNEL","SEND_MESSAGES","MANAGE_WEBHOOKS"],
+  category: "backup",
+  run: async (client, message, args) => {
+    let backupData = await backup.create(message.guild, {
+      jsonBeautify: true,
+      saveImages: "base64"
+    });
+    let embed = new client.Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription(
+        `Server: ${message.guild.name}\nThe backup has been created! To load it, type this command on the server of your choice: \`<prefix>load-backup ${backupData.id}\``
+      )
+     .setFooter("Powered By Reflex Development");
+    client.send(null,{ embed: [embed], type:true, message});
+  }
+};
