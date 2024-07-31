108f750c2e5ea76713d5d1d24dd76a892e6011c8
ToxicDevil <93937235+ToxicDevilGamer@users.noreply.github.com>
2024-07-31 10:10:12 +0530

    Add files via upload

---
+++ commands/ADMINISTRATION/lock.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'lock',
+  aliases: ["lock", "l"],
+  description: "Locks Any Channel",
+  userPerms: ["MANAGE_CHANNELS"],
+  botPerms: ["MANAGE_CHANNELS"],
+  category: "Moderation",
+  cooldown: 5,
+
+
+
+  run: async (client, message, args, prefix) => {
+    //Code
+
+    if (message.member.permissions.has('MANAGE_CHANNELS'));
+    {}
+
+    const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
+    if (!channel) {
+      return message.reply('Could not find channel.');
+    }
+
+    // Overwrite the channel permissions to deny everyone the SEND_MESSAGES permission.
+    channel.overwritePermissions([
+      {
+        id: message.guild.id,
+        deny: ['SEND_MESSAGES'],
+      },
+    ]);
+
+    return message.reply('Channel has been locked.');
+  },
+};
