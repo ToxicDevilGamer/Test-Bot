efafc0b91fadf2b4f12090508eef6731b5d2c9fb
ToxicDevil <93937235+ToxicDevilGamer@users.noreply.github.com>
2024-07-31 17:09:33 +0000

    ETC. delete

--- commands/NQN/howtodo.js
+++ commands/NQN/howtodo.js
@@ -5,25 +5,25 @@ module.exports = {
   category: "nqn",
   description: "How To Use Nqn /Free Nitro Emoji Features",
   run: async (client, message, args) => {
-    
+
     let embed1 = new discord.MessageEmbed()
     .setTitle(`Type the image`)
     .setColor("RANDOM")
     .setImage("https://raw.githubusercontent.com/Crypto195/NQN_Remake/main/before.PNG")
     .setfooter(Powered By Reflex Development)
     .setTimestamp(message.timestamp = Date.now())
-    
+
     message.channel.send(embed1);
-    
+
      let embed2 = new discord.MessageEmbed()
     .setTitle(`Send and Hurray!`)
     .setColor("RANDOM")
     .setImage("https://raw.githubusercontent.com/Crypto195/NQN_Remake/main/after.PNG")
     .setfooter(Powered By Reflex Development)
     .setTimestamp(message.timestamp = Date.now())
-    
+
     message.channel.send(embed2)
-    
-  
+
+
   }
 }
